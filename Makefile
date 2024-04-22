fc5a23576d5164691713ef264a29002d1852aa82
Clara Prando <clara@macbook-air-de-clara.home>
2024-04-22 17:02:36 +0200

    hello printf

---
+++ Makefile
@@ -0,0 +1,54 @@
+NAME			=	libftprintf.a
+
+CC				=	gcc
+CFLAGS			=	-Wall -Wextra -Werror
+AR				=	ar
+ARFLAGS 		=	rcs
+RM				=	rm -rf
+
+SRC				=	ft_hex_len \
+					ft_int_len \
+					ft_print_char \
+					ft_print_int \
+					ft_print_p \
+					ft_print_str \
+					ft_print_u \
+					ft_print_x \
+					ft_printf \
+					ft_printux \
+					ft_print_percent \
+					ft_ui_len
+SRCS 			=	$(addsuffix .c, $(SRC))
+
+OBJ_DIR			=	obj
+OBJS			=	$(SRCS:%.c=$(OBJ_DIR)/%.o)
+
+LIBFT_PATH		=	./libft
+LIBFT			=	$(LIBFT_PATH)/libft.a
+
+$(OBJ_DIR)/%.o:		%.c
+					$(CC) $(CFLAGS) -c $< -o $@
+
+all:				$(NAME)
+
+$(NAME):			$(LIBFT) $(OBJ_DIR) $(OBJS)
+				cp	$(LIBFT) $(NAME)
+					$(AR) $(ARFLAGS) $(NAME) $(OBJS)
+
+$(LIBFT):
+					make -C $(LIBFT_PATH) all
+
+$(OBJ_DIR):
+					mkdir -p $(OBJ_DIR)
+
+clean:
+					make -C $(LIBFT_PATH) clean
+					$(RM) $(OBJ_DIR)
+
+fclean:				clean
+					make -C $(LIBFT_PATH) fclean
+					$(RM) $(NAME)
+
+re:					fclean all
+
+.PHONY:				all bonus clean fclean re libft
---
+++ ft_print_p.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_p.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: claprand <claprand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 12:45:45 by claprand          #+#    #+#             */
+/*   Updated: 2024/04/22 16:59:14 by claprand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_print_p(void *ptr, int *len)
+{
+	unsigned long long	address;
+	char				*digits;
+
+	address = (unsigned long long)ptr;
+	digits = "0123456789abcdef";
+	if (address == 0)
+	{
+		write (1, "(nil)", 5);
+		*len = 5;
+		return (*len);
+	}
+	if (address >= 16)
+	{
+		*len = ft_print_p((void *)(address / 16), len);
+		*len = ft_print_p((void *)(address % 16), len);
+	}
+	else
+	{
+		ft_putchar_fd(digits[address], 1);
+		(*len)++;
+	}
+	return (*len);
+}
